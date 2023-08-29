eba8ed693e26462a01f4cda3843d01ea35d18a22
Tolulope <debbyonasanya@gmail.com>
2023-08-28 19:56:55 -0700

    update

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for x in range(0, list_length):
+        try:
+            result = my_list_1[x] / my_list_2[x]
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new.append(result)
+    return (new)
